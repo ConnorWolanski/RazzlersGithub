d56396276b737ac10e822a48f27b324a8800c1c4
Scoutzknifez <Cody@4alexanders.com>
2019-03-18 16:14:09 -0700

    Testing video player

--- client/src/pages/PlayVideo.js
+++ client/src/pages/PlayVideo.js
@@ -1,12 +1,20 @@
 import React from "react";
 import '../style.css';
+
 class PlayVideo extends React.Component {
   render() {
     return (
       <div>
-      <h2><font color ="white" size = "50">Cody please suck my peepee</font></h2>
+      <h2><font color ="white" size = "50">Test</font></h2>
+
+      <video width="320" height="240" controls>
+        <source src="//Razzlers.me///var/www/Razzlers_files/videos/movies/20.mp4" type="video/mp4"/>
+      </video>
+
+
       </div>
     );
   }
 }
+
 export default PlayVideo
