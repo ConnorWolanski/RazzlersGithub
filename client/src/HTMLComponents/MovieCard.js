0f41eac0f61ee3cc2a8baccc45c40ab8d67379ca
ConnorWolanski <ronnocool222@gmail.com>
2019-03-19 20:17:56 -0700

    movie cards

--- client/src/HTMLComponents/MovieCard.js
+++ client/src/HTMLComponents/MovieCard.js
@@ -26,52 +26,12 @@ class MovieCard extends React.Component {
     super(props);
     // Track if the mouse hovering over the movie card
     this.state = {
-      isMouseOver: false,
-      isMovie: false,
-      movieid: this.this.props,
-      name: "",
-      description: "",
-      rating: 0,
-      actors: "",
-      release_year: 0
+      isMouseOver: false
     };
   }
-  checkParams().then(json => {
-    this.setState({
-      isMovie: json.isMovie,
-      id: json.id,
-      name: "",
-      description: "",
-      rating: 0,
-      actors: "",
-      release_year: 0
-    });
-    var isMovie = json.isMovie;
-    var id = json.id;
-    var set = {};
-    getVideoInfo(isMovie, id).then(result => {
-      set = result;
-      if(set.hasOwnProperty("result"))
-      {
-        // result from server didnt pull the correct file and it doesnt exist!
-        console.log("file doesnt exist!");
-      } else {
-        // parse good response into constants for current state
-        // {title, desc, rate, act, year};
-        this.setState({
-          isMovie: json.isMovie,
-          id: json.id,
-          name: set.title,
-          description: set.desc,
-          rating: set.rate,
-          actors: set.act,
-          release_year: set.year
-        });
-      }
-    });
-  });
-}
+
   render() {
+    const {movie} = this.props;
     // The subtitle won't render if it's null
     const subtitle = this.state.isMouseOver ? movie.overview : null;
 
--- client/src/pages/Movies.js
+++ client/src/pages/Movies.js
@@ -1,10 +1,43 @@
 import React from "react";
 import '../style.css';
+import MovieList from "../HTMLComponents/MovieList";
+import * as scrollHelpers from '../Helpers/ScrollHelpers';
 class Movies extends React.Component {
+  // constructor(props) {
+  //   super(props);
+  //   this.state = {
+  //     currentPage: 1
+  //   };
+  //   this.handleScroll = this.handleScroll.bind(this);
+  // }
+  //
+  // componentDidMount() {
+  //   window.onscroll = this.handleScroll;
+  //   this.props.getTopMovies(this.state.currentPage);
+  // }
+  //
+  // componentWillUnmount() {
+  //   window.removeEventListener('scroll', this.handleScroll);
+  // }
+  //
+  // handleScroll() {
+  //   const {topMovies} = this.props;
+  //   if (!topMovies.isLoading) {
+  //     let percentageScrolled = scrollHelpers.getPercentageScrolledDown(window);
+  //     if (percentageScrolled > .8) {
+  //       const nextPage = this.state.currentPage + 1;
+  //       this.props.getTopMovies(nextPage);
+  //       this.setState({currentPage: nextPage});
+  //     }
+  //   }
+  // }
+//  <MovieList movies={movies}/>
   render() {
+    const movies;
     return (
       <div>
       <h2><font color ="white" size = "50">Movies</font></h2>
+      <MovieCard movie={movie} />
       <button className="button2" onClick={() => window.location.href='PlayVideo?isMovie=true&id=1'}>PlayVideo</button>
       </div>
     );
